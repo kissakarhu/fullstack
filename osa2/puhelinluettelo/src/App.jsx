b90a3649e623b075374984d9e022c36115f195ea
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-03-26 14:27:57 +0200

    Lisää kontaktien suodattaminen

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'
-     }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const contactsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const addName = (event) => {
     event.preventDefault()
@@ -29,9 +37,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+    setShowAll(event.target.value === '')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with: 
+        <input
+          value={filter}
+          onChange={handleFilterChange}/>
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: 
@@ -50,7 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {contactsToShow.map(person =>
           <p key={person.name}>{person.name} {person.number}</p>
          )}
       </ul>
