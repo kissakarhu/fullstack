1cbf3f74e422c6dd18304e95aceeb8406b4bb4fc
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-04-03 12:12:55 +0300

    Tee frontendistä tuotantoversio

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -8,6 +8,7 @@ morgan.token('body', (request) => {
     return request.method === 'POST' ? JSON.stringify(request.body) : ''
 })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
