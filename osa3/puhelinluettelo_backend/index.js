6addeff96cd4a7ed4da9fa6eb4023e30fe335e58
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-04-03 17:11:05 +0300

    Poista tarpeeton taulukko

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -13,29 +13,6 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let numbers = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 const generateId = () => {
     return Math.floor(Math.random() * 1000000).toString()
 }
