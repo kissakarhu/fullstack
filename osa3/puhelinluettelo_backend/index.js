594961f94469a9c5a547cf0c5f551b542ab92269
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-04-04 12:05:02 +0300

    Korjaa ESLint virheet

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -6,7 +6,7 @@ const Person = require('./models/person')
 const app = express()
 
 morgan.token('body', (request) => {
-    return request.method === 'POST' ? JSON.stringify(request.body) : ''
+  return request.method === 'POST' ? JSON.stringify(request.body) : ''
 })
 
 app.use(express.static('dist'))
@@ -14,102 +14,102 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
-    return Math.floor(Math.random() * 1000000).toString()
+  return Math.floor(Math.random() * 1000000).toString()
 }
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(result => {
-        response.json(result)
-    })
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.get('/info', (request, response) => {
-    const time = new Date()
-    Person.countDocuments({})
-        .then(count => {
-            response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
-        })
+  const time = new Date()
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.deleteOne({ _id: request.params.id })
-    .then(result => {
-        response.status(204).end()
+  Person.deleteOne({ _id: request.params.id })
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            'error': 'name or number missing'
-        })
-    }
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: generateId()
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      'error': 'name or number missing'
     })
-    
-    person.save()
-        .then(result => response.json(result))
-        .catch(error => next(error))
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  })
+
+  person.save()
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    if (!name || !number) {
-        return response.status(400).json({ error: 'name or number missing' })
-    }
+  if (!name || !number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
 
-    const updatedPerson = { name, number }
+  const updatedPerson = { name, number }
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        updatedPerson,
-        { new: true}
-    )
+  Person.findByIdAndUpdate(
+    request.params.id,
+    updatedPerson,
+    { new: true }
+  )
     .then(updatedPerson => {
-        if (!updatedPerson) {
-            return response.status(404).json({ error: 'Person not found' })
-        }
-        response.json(updatedPerson)
+      if (!updatedPerson) {
+        return response.status(404).json({ error: 'Person not found' })
+      }
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+  console.log(`Server running on port ${PORT}`)
+})
 
--- osa3/puhelinluettelo_backend/models/person.js
+++ osa3/puhelinluettelo_backend/models/person.js
@@ -5,29 +5,27 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 console.log('Connecting to', url)
 mongoose.connect(url)
-.then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3
-    },
-    number: String,
+  name: {
+    type: String,
+    minlength: 3
+  },
+  number: String,
 })
 
-const Person = mongoose.model('Person', personSchema)
-
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
