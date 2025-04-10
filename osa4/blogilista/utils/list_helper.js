36585399ec68efb27cf8c6cde3db5efc10766014
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-04-10 16:25:02 +0300

    Lisää yksikkötesti favoriteBlog-funktiolle

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,7 +11,23 @@ const totalLikes = (blogs) => {
         ? 0
         : blogs.reduce(reducer, 0) 
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    let favorite = blogs[0]
+
+    blogs.forEach(blog => {
+        if (blog.likes > favorite.likes) {
+            favorite = blog
+        }
+    })
+
+    return favorite
+}
   
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
