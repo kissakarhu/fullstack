882eea7b1af96a8f0f9c9c8d09c9d9eaf55f9eb8
kissakarhu <petra.pitkamaki@hotmail.fi>
2025-04-14 14:32:30 +0300

    Lisää testi POST-pyynnölle

--- osa4/blogilista/tests/bloglist_api.test.js
+++ osa4/blogilista/tests/bloglist_api.test.js
@@ -11,7 +11,7 @@ test('GET returns correct amount of blogs as JSON', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/)
     
-    const expectedBlogCount = 6
+    const expectedBlogCount = 16
     if (response.body.length !== expectedBlogCount) {
         throw new Error(`Expected ${expectedBlogCount} blogs, got ${response.body.length}`)
     }
@@ -29,9 +29,38 @@ test('blogs have id field, not _id', async () => {
         if (blog._id) {
             throw new Error('Blog should not contain _id field')
         }
-    })
-        
+    }) 
+})
+
+test('a new blog can be added with POST request', async () => {
+    const initialResponse = await api.get('/api/blogs')
+    const initialBlogs = initialResponse.body
+
+    const newBlog = {
+        title: 'Uusi blogi',
+        author: 'Testaaja',
+        url: 'http://example.com/testi',
+        likes: 8
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const responseAfter = await api.get('/api/blogs')
+    const blogsAfter = responseAfter.body
+
+    if (blogsAfter.length !== initialBlogs.length + 1) {
+        throw new Error('Blog count did not grow as expected')
+    }
     
+    const titles = blogsAfter.map(blog => blog.title)
+    if (!titles.includes(newBlog.title)) {
+        throw new Error('New blog cannot be found from the bloglist')
+    }
+
 })
 
 after(async () => {
